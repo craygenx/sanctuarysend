7e57244442012bb93598535ad409f8e2036f09ec
craygenx <marshmellowking99@gmail.com>
2024-02-14 18:17:17 +0300

    updated the payment UI

--- lib/Layout/Payment_layout/paymentDesktop_layout.dart
+++ lib/Layout/Payment_layout/paymentDesktop_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sanctuarysend/widgets/custom_bold_txt.dart';
 
 import '../../widgets/main_appbar.dart';
@@ -16,88 +17,134 @@ class _PaymentDesktopLayoutState extends State<PaymentDesktopLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomMainAppbar(),
-      body: Container(
+      body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        color: Colors.pink,
+        // color: Colors.pink,
         child: Row(
           children: [
-            Container(
+            SizedBox(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width * 1/2,
-              color: Colors.blue,
+              // color: Colors.blue,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  const BoldText(text: 'Payment'),
+                  // const BoldText(text: 'Payment'),
                   SizedBox(
                     child: Column(
                       children: [
-                        const SizedBox(
-                          width: 300,
-                          height: 300,
-                          child: Placeholder(),
-                        ),
-                        SizedBox(
-                          child: Row(
-                            children: [
-                              Expanded(child: Container(
-                                color: Colors.purple,
-                                child: const BoldText(text: 'Cash'),
-                              )),
-                              Expanded(child: Container(
-                                color: Colors.red,
-                                child: const BoldText(text: 'Mobile Money'),
-                              )),
-                            ],
+                        const Padding(
+                          padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                          child: SizedBox(
+                            width: 300,
+                            height: 300,
+                            child: Placeholder(),
                           ),
                         ),
-                        Container(
-                          width: 200,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color:  Colors.black,
-                                width: 1.0,
-                              )
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                          child: SizedBox(
+                            child: Row(
+                              children: [
+                                Expanded(child: Container(
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    border: Border.all(
+                                      color: Colors.black
+                                    )
+                                  ),
+                                  // color: Colors.purple,
+                                  child: const BoldText(text: 'Cash'),
+                                )),
+                                Expanded(child: Container(
+                                  height: 50,
+                                  // color: Colors.red,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.black
+                                      )
+                                  ),
+                                  child: const BoldText(text: 'Mobile Money'),
+                                )),
+                              ],
                             ),
-                            // color: Colors.blueGrey,
-                            child: const TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.zero)
-                                )
-                              ),
-                            ),
+                          ),
                         ),
-                        ElevatedButton(
-                            onPressed: (){}, 
-                            child: const BoldText( text: 'Send Payment',)),
+                        FractionallySizedBox(
+                          widthFactor: .75,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 20.0, top: 20.0),
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color:  Colors.black,
+                                    width: 1.0,
+                                  )
+                                ),
+                                // color: Colors.blueGrey,
+                                child: const TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(Radius.zero)
+                                    )
+                                  ),
+                                ),
+                            ),
+                          ),
+                        ),
+                        FractionallySizedBox(
+                          widthFactor: .75,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                  )
+                                ),
+                                child: const BoldText( text: 'Send Payment',)),
+                          ),
+                        ),
                       ],
                     ),
                   )
                 ],
               ),
             ),
-            Container(
+            SizedBox(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width * 1/2,
-              color: Colors.yellow,
+              // color: Colors.yellow,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(
-                    child: Row(
-                      children: [
-                        Icon(Icons.monetization_on_outlined),
-                        BoldText(text: 'Ksh: 20000.00')
-                      ],
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 20.0),
+                            child: FaIcon(FontAwesomeIcons.sackDollar),
+                          ),
+                          BoldText(text: 'Ksh: 20000.00', fontSize: 22,)
+                        ],
+                      ),
                     ),
                   ),
                   SizedBox(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const BoldText(text: 'Payment Info'),
-                        const BoldText(text: 'payment method'),
+                        const BoldText(text: 'Payment Info', fontSize: 22,),
+                        const Text('payment method'),
                         SizedBox(
                           child: Column(
                             children: [
@@ -132,17 +179,24 @@ class _PaymentDesktopLayoutState extends State<PaymentDesktopLayout> {
                             ],
                           ),
                         ),
-                        const BoldText(text: "Sender's Account Name"),
-                        const BoldText(text: 'John Doe'),
+                        // const BoldText(text: "Sender's Account Name"),
+                        const Text("Sender's Account Name"),
+                        // const BoldText(text: 'John Doe'),
+                        const Text('John Doe',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         const SizedBox(
                           child: Column(
                             children: [
-                              BoldText(text: 'BreakDown'),
+                              BoldText(text: 'BreakDown', fontSize: 22,),
                               SizedBox(
                                 child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
-                                    BoldText(text: 'Mothers Union'),
-                                    BoldText(text: '3,000.00')
+                                    BoldText(text: 'Mothers Union', fontWeight: FontWeight.normal, fontSize: 18,),
+                                    BoldText(text: '3,000.00', fontSize: 22,)
                                   ],
                                 ),
                               )
--- lib/Layout/Payment_layout/paymentMobi_layout.dart
+++ lib/Layout/Payment_layout/paymentMobi_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../widgets/custom_bold_txt.dart';
 import '../../widgets/main_appbar.dart';
@@ -16,130 +17,201 @@ class _PaymentMobiLayoutState extends State<PaymentMobiLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomMainAppbar(),
-      body: SizedBox(
-        child: Column(
-          children: [
-            Container(
-              // height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.blue,
-              child: Column(
-                children: [
-                  const BoldText(text: 'Payment'),
-                  SizedBox(
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          child: Row(
-                            children: [
-                              Icon(Icons.monetization_on_outlined),
-                              BoldText(text: 'Ksh: 20000.00')
-                            ],
+      body: SingleChildScrollView(
+        child: SizedBox(
+          child: Column(
+            children: [
+              Container(
+                // height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: [
+                    const BoldText(text: 'Payment', fontSize: 18),
+                    SizedBox(
+                      child: Column(
+                        children: [
+                          Container(
+                            color: const Color.fromRGBO(242, 250, 253, 1),
+                            height: 50,
+                            child: const Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(right: 20.0),
+                                  child: FaIcon(FontAwesomeIcons.sackDollar),
+                                ),
+                                BoldText(text: 'Ksh: 20000.00', fontSize: 22,)
+                              ],
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          child: Row(
-                            children: [
-                              Expanded(child: Container(
-                                height: 40,
-                                color: Colors.purple,
-                                child: const BoldText(text: 'Cash'),
-                              )),
-                              Expanded(child: Container(
-                                height: 40,
-                                color: Colors.red,
-                                child: const BoldText(text: 'Mobile Money'),
-                              )),
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                            child: SizedBox(
+                              child: Row(
+                                children: [
+                                  Expanded(child: Container(
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey,
+                                        border: Border.all(
+                                            color: Colors.black
+                                        )
+                                    ),
+                                    // color: Colors.purple,
+                                    child: const BoldText(text: 'Cash'),
+                                  )),
+                                  Expanded(child: Container(
+                                    height: 50,
+                                    // color: Colors.red,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.black
+                                        )
+                                    ),
+                                    child: const BoldText(text: 'Mobile Money'),
+                                  )),
+                                ],
+                              ),
+                            ),
                           ),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                color:  Colors.black,
-                                width: 1.0,
-                              )
-                          ),
-                          // color: Colors.blueGrey,
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.all(Radius.zero)
-                                )
+                        FractionallySizedBox(
+                          widthFactor: .95,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 20.0, top: 20.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color:  Colors.black,
+                                    width: 1.0,
+                                  )
+                              ),
+                              // color: Colors.blueGrey,
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(Radius.zero)
+                                    )
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                        ElevatedButton(
-                            onPressed: (){},
-                            child: const BoldText( text: 'Send Payment',)),
-                        SizedBox(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const BoldText(text: 'Payment Info'),
-                              const BoldText(text: 'payment method'),
-                              SizedBox(
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Radio(
-                                            value: 1,
-                                            groupValue: selectedValue,
-                                            onChanged: (value){
-                                              setState(() {
-                                                selectedValue = value!;
-                                              });
-                                            }),
-                                        const BoldText(text: 'Mobile Money'),
-                                      ],
-                                    ),
-
-                                    Row(
-                                      children: [
-                                        Radio(
-                                            value: 2,
-                                            groupValue: selectedValue,
-                                            onChanged: (value){
-                                              setState(() {
-                                                selectedValue = value!;
-                                              });
-                                            }),
-                                        const BoldText(text: 'Cash'),
-                                      ],
-                                    ),
-
-                                  ],
-                                ),
-                              ),
-                              const BoldText(text: "Sender's Account Name"),
-                              const BoldText(text: 'John Doe'),
-                              const SizedBox(
-                                child: Column(
-                                  children: [
-                                    BoldText(text: 'BreakDown'),
-                                    SizedBox(
-                                      child: Row(
-                                        children: [
-                                          BoldText(text: 'Mothers Union'),
-                                          BoldText(text: '3,000.00')
-                                        ],
-                                      ),
+                        FractionallySizedBox(
+                          widthFactor: .95,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.blue,
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.zero,
                                     )
-                                  ],
                                 ),
-                              )
-                            ],
+                                child: const BoldText( text: 'Send Payment',)),
                           ),
                         ),
-                      ],
-                    ),
-                  )
-                ],
+                          SizedBox(
+                            // height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width,
+                            // color: Colors.yellow,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // const Padding(
+                                //   padding: EdgeInsets.only(bottom: 20.0),
+                                //   child: SizedBox(
+                                //     height: 50,
+                                //     child: Row(
+                                //       children: [
+                                //         Padding(
+                                //           padding: EdgeInsets.only(right: 20.0),
+                                //           child: FaIcon(FontAwesomeIcons.sackDollar),
+                                //         ),
+                                //         BoldText(text: 'Ksh: 20000.00', fontSize: 22,)
+                                //       ],
+                                //     ),
+                                //   ),
+                                // ),
+                                SizedBox(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      const BoldText(text: 'Payment Info', fontSize: 18,),
+                                      const Text('payment method'),
+                                      SizedBox(
+                                        child: Column(
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Radio(
+                                                    value: 1,
+                                                    groupValue: selectedValue,
+                                                    onChanged: (value){
+                                                      setState(() {
+                                                        selectedValue = value!;
+                                                      });
+                                                    }),
+                                                const BoldText(text: 'Mobile Money'),
+                                              ],
+                                            ),
+        
+                                            Row(
+                                              children: [
+                                                Radio(
+                                                    value: 2,
+                                                    groupValue: selectedValue,
+                                                    onChanged: (value){
+                                                      setState(() {
+                                                        selectedValue = value!;
+                                                      });
+                                                    }),
+                                                const BoldText(text: 'Cash'),
+                                              ],
+                                            ),
+        
+                                          ],
+                                        ),
+                                      ),
+                                      // const BoldText(text: "Sender's Account Name"),
+                                      const Text("Sender's Account Name"),
+                                      // const BoldText(text: 'John Doe'),
+                                      const Text('John Doe',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        child: Column(
+                                          children: [
+                                            BoldText(text: 'BreakDown', fontSize: 18,),
+                                            SizedBox(
+                                              child: Row(
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                children: [
+                                                  BoldText(text: 'Mothers Union', fontWeight: FontWeight.normal, fontSize: 18,),
+                                                  BoldText(text: '3,000.00', fontSize: 22,)
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

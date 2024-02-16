2e4c854171d662c33d0899e81cec7441aa6b011e
craygenx <marshmellowking99@gmail.com>
2024-02-16 11:49:47 +0300

    added firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sanctuarysend/Layout/Otp_layout/otpdesktop_layout.dart';
 import 'package:sanctuarysend/Responsive/signin_breakpoint.dart';
 
 import 'Layout/Signin_layout/signinmobi_layout.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
